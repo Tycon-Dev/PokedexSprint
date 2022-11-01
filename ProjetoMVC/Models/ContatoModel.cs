d6c106b477fb038e9f97be75aeaa9bd0248f86c4
tylar <tylar@IKO>
2022-11-01 18:29:10 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMVC/Models/ContatoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoMVC.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string Evolui { get; set; }
+    }
+}
